c93e72530577cd42499d6731dcb0fd871b434348
Sylvance <kerandisylvance@gmail.com>
2017-10-18 19:11:42 +0300

    Final changes

--- prodsum.py
+++ prodsum.py
@@ -1,14 +1,13 @@
 """ This is the missing docstring "LINTER" is looking for"""
-class Diffps(object):
+class Prodsum(object):
+    """Prodsum."""
 
     def add(self, arg1, arg2):
         """Return the sum of two arguments."""
         result1 = arg1 + arg2
-        self.ans1 = result1
         return result1
 
     def multiply(self, arg1, arg2):
         """Return the product of two arguments."""
         result2 = arg1 * arg2
-        self.ans2 = result2
         return result2
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 """ Test cases """
 import unittest
-from diffps import Diffps
+from prodsum import Prodsum
 
 
 class BasicTest(unittest.TestCase):
     """ Basic tests """
     def test_add(self):
         """ Addition test"""
-        calc = Diffps()
+        calc = Prodsum()
         result = calc.add(2, 3)
         self.assertEqual(5, result)
 
     def test_product(self):
         """ Product test"""
-        calc = Diffps()
+        calc = Prodsum()
         result = calc.multiply(2, 3)
         self.assertEqual(6, result)
 
