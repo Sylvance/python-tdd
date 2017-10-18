259181e2872ddc0e2e5ca05c7ca8c5ab0b1916c0
Sylvance <kerandisylvance@gmail.com>
2017-10-18 17:53:36 +0300

    Add more changes

--- diffps.py
+++ diffps.py
@@ -1,8 +1,15 @@
+""" This is the missing docstring "LINTER" is looking for"""
 class Diffps(object):
+    """Return the difference between the product and the sum."""
+
     def add(self, arg1, arg2):
+        """Return the sum of two arguments."""
         pass
+
     def multiply(self, arg1, arg2):
+        """Return the product of two arguments."""
         pass
+
     def result(self, ans1, ans2):
+        """Return the difference two arguments."""
         pass
-    
---
+++ test.py
@@ -0,0 +1,22 @@
+""" Test cases """
+import unittest
+from diffps import Diffps
+
+
+class BasicTest(unittest.TestCase):
+    """ Basic tests """
+    def test_add(self):
+        """ Addition test"""
+        calc = Diffps()
+        result = calc.add(1, 1)
+        self.assertEqual(2, result)
+
+    def test_product(self):
+        """ Product test"""
+        calc = Diffps()
+        result = calc.multiply(2, 3)
+        self.assertEqual(6, result)
+
+if __name__ == '__main__':
+    unittest.main()
+    
